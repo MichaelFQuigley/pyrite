e99061e0aecc1f0cdee4ff9c7dd8b0d154fe72cf
Michael Quigley <mquigley@cs.utah.edu>
2016-05-31 16:48:39 -0600

    move llvm submod

---
+++ Lexers/pretty-print.sh
@@ -0,0 +1,1 @@
+python -m json.tool | pygmentize -l json
---
+++ Lexers/util.ml
@@ -0,0 +1,8 @@
+open Unix;;
+
+let debug_print str = 
+    if false then
+    begin
+        print_string str;
+        print_string "\n";
+    end;;
